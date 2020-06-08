364f917070768034a4aef779fc124a328a54ad01
Jeong Seongil <develop.mari@gmail.com>
2020-06-08 23:58:57 +0900

    리스트 레이아웃 수정

--- view.py
+++ view.py
@@ -8,31 +8,83 @@ class FileView(QWidget):
     defaultThumbnail = "thumb/sample.jpg"
 
     thumbnail = None
-    label = None
+    name = None
+    tags = None
+    rate = None
 
     def __init__(self, root):
         super().__init__()
-        layout = QVBoxLayout()
+        layout = QGridLayout()
+
+        background = QWidget()
+        background.setStyleSheet('''
+            background-image: url(./thumb/sample.jpg);
+            background-position: center center;''')
+        # bgEffect = QGraphicsEffect(background)
+        # bgEffect.setOpacity(0.3)
+        bgEffect = QGraphicsOpacityEffect()
+        bgEffect.setOpacity(0.3)
+        background.setGraphicsEffect(bgEffect)
+        layout.addWidget(background, 0, 0)
+
+        content = QHBoxLayout()
 
         self.thumbnail = QLabel(root)
-        self.thumbnail.resize(100, 100)
-        self.thumbnail.setMinimumSize(100, 100)
+        self.thumbnail.setFixedWidth(100)
+        self.thumbnail.setFixedHeight(100)
+        self.thumbnail.setMargin(0)
         self.setThumbnail(self.defaultThumbnail)
-        layout.addWidget(self.thumbnail)
+        content.addWidget(self.thumbnail)
         
-        self.label = QLabel(root)
-        f = self.label.font()
+        rightside = QVBoxLayout()        
+
+        self.name = QLabel(root)
+        self.name.setMargin(0)
+        self.name.setFixedWidth(500)
+        self.name.setFixedHeight(60)
+        self.name.setWordWrap(True)
+        f = self.name.font()
         f.setFamily('맑은 고딕')
-        f.setPointSize(15)
-        self.label.setFont(f)
-        layout.addWidget(self.label)
- 
-        # self.resize(100, 120)
+        f.setPointSize(12)
+        f.bold()
+        self.name.setFont(f)
+        rightside.addWidget(self.name)
+
+        rightside.addStretch(1)
+        
+        self.rate = QLabel(root)
+        f = self.rate.font()
+        f.setFamily('맑은 고딕')
+        f.setPointSize(10)
+        self.rate.setFont(f)
+        rightside.addWidget(self.rate)
+        
+
+        self.tags = QLabel(root)
+        self.tags.setMargin(0)
+        f = self.tags.font()
+        f.setFamily('맑은 고딕')
+        f.setPointSize(10)
+        self.tags.setFont(f)
+        rightside.addWidget(self.tags)
+
+        content.addLayout(rightside)
+        content.setSpacing(0)
+        layout.addLayout(content, 0, 0)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.resize(100, 120)
         self.setLayout(layout)
         # self.setMinimumSize(100, 100)
 
-    def setText(self, text):
-        self.label.setText(text)
+    def setName(self, text):
+        self.name.setText(text)
+    
+    def setRate(self, rate):
+        self.rate.setText(f'Rate : {str(rate)} / 5')
+
+    def setTags(self, tags):
+        self.tags.setText(f'Tag : {", ".join(tags)}')
     
     def setThumbnail(self, thumbnailPath):
         p = QPixmap(thumbnailPath)
@@ -51,7 +103,7 @@ class MyApp(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
-    def label(self):
+    def grid(self):
         conn, c = dbInterface.establish()
         files = dbInterface.getFiles(c)
 
@@ -60,7 +112,6 @@ class MyApp(QWidget):
         for column in range(int(len(files)/3)):
             for row in range(5):
                 if(column*3+row >= len(files)): continue
-
                 v = FileView(self)
                 v.setText(files[column*3+row].name)
                 grid.addWidget(v, column, row)
@@ -77,12 +128,37 @@ class MyApp(QWidget):
         self.setLayout(root)
         dbInterface.distroy(conn)
 
+    def list(self):
+        conn, c = dbInterface.establish()
+        files = dbInterface.getFiles(c)
+
+        li = QVBoxLayout()
+        li.setContentsMargins(0, 0, 0, 0)
+
+        for i in range(len(files)):
+                v = FileView(self)
+                v.setName(files[i].name)
+                v.setRate(files[i].rate)
+                v.setTags(files[i].tags)
+                li.addWidget(v)
+
+        content = QWidget()
+        content.setLayout(li)
+
+        scrollView = QScrollArea()
+        scrollView.setWidget(content)
+
+        root = QHBoxLayout()
+        root.addWidget(scrollView)
+
+        self.setLayout(root)
+        dbInterface.distroy(conn)
 
     def initUI(self):
         self.setWindowTitle('NSFW Bolt')
-        self.resize(800, 600)
+        self.resize(700, 600)
         self.center()
-        self.label()
+        self.list()
         self.show()
 
 
